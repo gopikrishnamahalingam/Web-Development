3fa3ca9a77208d4f65597526da08ba9657481b85
Gopikrishna Mahalingam <gopikrishna.mahalingam@gmail.com>
2026-09-18 21:23:47 +0530

    Add Statistics, Local Storage, and Enhanced UI in Expense Tracker project

--- Basic_JavaScript_Revision/Expense_Tracker/script.js
+++ Basic_JavaScript_Revision/Expense_Tracker/script.js
@@ -1,27 +1,43 @@
 // Expense Array
 let AllExpenses = [];
+let Exp_Id = (JSON.parse(localStorage.getItem("Exp_Id")) == null) ? 0 : Number(JSON.parse(localStorage.getItem("Exp_Id")));
 
-
+statsUpdate();
 // Validation Function
 function validation(x, p) {
-    if (x.name == "") {
-        p.textContent = "Name Cannot be empty!";
-        return -1;
-    }
-    else if (x.amount == "") {
-        p.textContent = "Amount Cannot be empty!";
-        return -1;
-    }
-    else if (x.date == "") {
-        p.textContent = "Enter a valid Date";
-        return -1;
-    }
+    // if (x.name == "") {
+    //     p.textContent = "Name Cannot be empty!";
+    //     return -1;
+    // }
+    // else if (x.amount == "") {
+    //     p.textContent = "Amount Cannot be empty!";
+    //     return -1;
+    // }
+    // else if (x.date == "") {
+    //     p.textContent = "Enter a valid Date";
+    //     return -1;
+    // }
 }
 
 
+const p = document.createElement("p");
+p.textContent = "No relevant content to show";
 const container = document.querySelector(".expenseContainer");
+if (AllExpenses.length == 0) {
+    container.append(p);
+}
 // Add to DOM Function
 function addExpenseToDOM(x) {
+
+    if (container.firstElementChild == p) {
+        container.innerHTML = "";
+        console.log("EVERY?");
+
+    }
+    statsUpdate();
+    saveExpenses();
+    // console.log("ADD");
+
     const card = document.createElement("div");
     card.classList.add("expenseCard");
 
@@ -96,15 +112,19 @@ function addExpenseToDOM(x) {
                 note: head.nextSibling.nextSibling.nextSibling.nextSibling.lastChild.value,
             }
 
-            // if(validation(exp,dialog.firstChild.lastChild) ==-1){
-            //     return;
-            // }
+            if (validation(exp, dialog.firstChild.lastChild) == -1) {
+                return;
+            }
 
             const expenseIndex = AllExpenses.findIndex((a) => a.expense_ID == findingExpense);
             label4.textContent = AllExpenses[expenseIndex].name = exp.name;
-            label6.textContent = AllExpenses[expenseIndex].amount = exp.amount;
+            label6.textContent = AllExpenses[expenseIndex].amount = Number(exp.amount);
             label8.textContent = AllExpenses[expenseIndex].date = exp.date;
             label10.textContent = AllExpenses[expenseIndex].note = exp.note;
+
+            statsUpdate();
+            saveExpenses();
+            // console.log("UPDATE");
 
             dialog.remove();
         })
@@ -118,19 +138,28 @@ function addExpenseToDOM(x) {
     delBtn.addEventListener("click", () => {
         const findingExpense = updBtn.parentElement.parentElement.firstChild.lastChild.textContent;
         AllExpenses.splice(AllExpenses.findIndex((a) => a.expense_ID == findingExpense), 1);
-        console.log(findingExpense);
-        console.log(AllExpenses);
+        // console.log(findingExpense);
+        // console.log(AllExpenses);
         updBtn.parentElement.parentElement.remove();
+        statsUpdate();
+        saveExpenses();
+        // console.log("Saved DELETE");
+        if (AllExpenses.length == 0) {
+            container.append(p);
+        }
+
 
     })
 
     card.append(div1, div2, div3, div4, div5, div6);
     container.append(card);
 }
-let Exp_Id = 0;
+
+
 // Add Expense 
 const addExpenseBtn = document.querySelector(".addExpenseBtn");
 addExpenseBtn.addEventListener('click', (e) => {
+
     const dialog = document.createElement("dialog");
     dialogCreation(dialog);
     dialog.classList.add("dialog");
@@ -156,9 +185,9 @@ addExpenseBtn.addEventListener('click', (e) => {
         // console.log(exp.name);
 
         // ******
-        // if (validation(exp , dialog.firstChild.lastChild) === -1) {
-        //     return;
-        // }
+        if (validation(exp, dialog.firstChild.lastChild) === -1) {
+            return;
+        }
         console.log("ADD VALIDATION");
 
         AllExpenses.push({
@@ -174,7 +203,7 @@ addExpenseBtn.addEventListener('click', (e) => {
         // input4.value = "";
         dialog.remove();
         addExpenseToDOM(AllExpenses[AllExpenses.length - 1]);
-        console.log(AllExpenses);
+        // console.log(AllExpenses);
     })
 
 
@@ -241,78 +270,141 @@ function dialogCreation(dialog) {
 const searchExpense = document.querySelector("#searchExpense");
 const searchExpenseBtn = document.querySelector(".searchExpenseBtn");
 
-searchExpenseBtn.addEventListener("click",()=>{
+searchExpenseBtn.addEventListener("click", () => {
     // if(searchExpense.value ==""){}
-    
+
     const filteredArray = AllExpenses.filter((a) => a.name.toLowerCase().includes(searchExpense.value.toLowerCase().trim()));
-    
+
     container.innerHTML = "";
     filteredArray.forEach(e => addExpenseToDOM(e));
-    
+    if (filteredArray.length == 0) {
+        container.append(p);
+    }
+
 })
 
 // Reset Expense
 const resetExpenseBtn = document.querySelector(".resetExpenseBtn");
-resetExpenseBtn.addEventListener("click",()=>{
-    Exp_Id =0;
-    container.innerHTML ="";
+resetExpenseBtn.addEventListener("click", () => {
+    Exp_Id = 0;
+    container.innerHTML = "";
     AllExpenses = [];
-    console.log(AllExpenses);
-    
+    // console.log(AllExpenses);
+    statsUpdate();
+    saveExpenses();
+    container.append(p);
+    // console.log("RESET");
+
 })
 
 // Expense Statistic
+const Statistics = document.querySelector(".Statistics");
+function statsUpdate() {
+    const statsUpdateArray = [...AllExpenses].sort((a, b) => a.amount - b.amount);
 
+    const totalExpense = AllExpenses.reduce((S, C) => {
+        return S = S + C.amount
+    }, 0);
+
+    const label1 = document.querySelector(".totalNoOfExpense")
+    label1.textContent = AllExpenses.length;
+
+    const label2 = document.querySelector(".totalExpense")
+    label2.textContent = "";
+    label2.textContent = totalExpense;
+
+    const label3 = document.querySelector(".highestExpense")
+    const label4 = document.querySelector(".highestExpenseBy")
+    const label5 = document.querySelector(".lowestExpense")
+    const label6 = document.querySelector(".lowestExpenseBy")
+    if (statsUpdateArray.length == 0) {
+        label3.textContent = label4.textContent = label5.textContent = label6.textContent = "NIL";
+    }
+    else {
+        label3.textContent = statsUpdateArray[statsUpdateArray.length - 1].amount;
+
+        label4.textContent = statsUpdateArray.filter((a) => a.amount == label3.textContent).map(a => a.name);
+
+        label5.textContent = statsUpdateArray[0].amount;
+
+        label6.textContent = statsUpdateArray.filter((a) => a.amount == label5.textContent).map(a => a.name);
+    }
+
+}
 
 // Expense Filter
 const sortByFilter = document.querySelector("#sortByFilter");
 
-sortByFilter.addEventListener("change",()=>{
-   let sortByFilterArray;
-   switch (sortByFilter.value) {
-    case "reset":{
-        sortByFilterArray = AllExpenses;
-        break;
+sortByFilter.addEventListener("change", () => {
+    let sortByFilterArray;
+    switch (sortByFilter.value) {
+        case "reset": {
+            sortByFilterArray = AllExpenses;
+            break;
+        }
+        case "NameAsc": {
+            sortByFilterArray = [...AllExpenses].sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+            break;
+        }
+        case "NameDesc": {
+            sortByFilterArray = [...AllExpenses].sort((a, b) => b.name.toLowerCase().localeCompare(a.name.toLowerCase()));
+            break;
+        }
+        case "AmountLH": {
+            sortByFilterArray = [...AllExpenses].sort((a, b) => a.amount - b.amount);
+            break;
+        }
+        case "AmountHL": {
+            sortByFilterArray = [...AllExpenses].sort((a, b) => b.amount - a.amount);
+            break;
+        }
+        case "NtO": {
+            sortByFilterArray = [...AllExpenses].sort((a, b) => b.date.toLowerCase().localeCompare(a.date.toLowerCase()));
+            break;
+        }
+        case "OtN": {
+            sortByFilterArray = [...AllExpenses].sort((a, b) => a.date.toLowerCase().localeCompare(b.date.toLowerCase()));
+            break;
+        }
     }
-    case "NameAsc":{
-        sortByFilterArray = [...AllExpenses].sort((a,b)=> a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-        break;
-    }
-    case "NameDesc":{
-        sortByFilterArray = [...AllExpenses].sort((a,b)=> b.name.toLowerCase().localeCompare(a.name.toLowerCase()));
-        break;
-    }
-    case "AmountLH":{
-        sortByFilterArray = [...AllExpenses].sort((a,b)=> a.amount-b.amount);
-        break;
-    }
-    case "AmountHL":{
-        sortByFilterArray = [...AllExpenses].sort((a,b)=> b.amount-a.amount);
-        break;
-    }
-    case "NtO":{
-        sortByFilterArray = [...AllExpenses].sort((a,b)=> b.date.toLowerCase().localeCompare(a.date.toLowerCase()));
-        break;
-    }
-    case "OtN":{
-        sortByFilterArray = [...AllExpenses].sort((a,b)=> a.date.toLowerCase().localeCompare(b.date.toLowerCase()));
-        break;
-    }
-   } 
-   container.innerHTML="";
-   sortByFilterArray.forEach(e=> addExpenseToDOM(e));
+    container.innerHTML = "";
+    sortByFilterArray.forEach(e => addExpenseToDOM(e));
 })
 
 const sortByDate = document.querySelector("#sortByDate");
-sortByDate.addEventListener("change",()=>{
-    const sortByDateArray = AllExpenses.filter((a)=> a.date == sortByDate.value.trim());
+sortByDate.addEventListener("change", () => {
+    const sortByDateArray = AllExpenses.filter((a) => a.date == sortByDate.value.trim());
 
-    container.innerHTML="";
-    sortByDateArray.forEach(e=>addExpenseToDOM(e));
+    container.innerHTML = "";
+    sortByDateArray.forEach(e => addExpenseToDOM(e));
+    if (sortByDateArray.length == 0) {
+        container.append(p);
+    }
 })
 
 const resetAllFilters = document.querySelector(".resetAllFilters");
-resetAllFilters.addEventListener("click",()=>{
-    container.innerHTML="";
-    AllExpenses.forEach(e=>addExpenseToDOM(e));
+resetAllFilters.addEventListener("click", () => {
+    container.innerHTML = "";
+    AllExpenses.forEach(e => addExpenseToDOM(e));
+    searchExpense.value = "";
+    // sortByDate.reset();
+    // sortByFilter.reset();
 })
+
+
+function saveExpenses() {
+    localStorage.setItem("Expenses", "");
+    localStorage.setItem("Expenses", JSON.stringify(AllExpenses));
+    // console.log("SAVED");
+    localStorage.setItem("Exp_Id", JSON.stringify(Exp_Id));
+}
+
+function loadStudents() {
+    AllExpenses = JSON.parse(localStorage.getItem("Expenses")) || [];
+    // console.log("HI");
+    // console.log("LOADINg");
+    // console.log(localStorage.getItem("Expenses"))
+    AllExpenses.forEach(e => addExpenseToDOM(e));
+
+}
+loadStudents();
